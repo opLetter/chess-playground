b82669294ae96f84c033449e60a89241d8cad036
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2023-07-12 18:21:19 -0400

    Initial Commit

---
+++ site/src/jsMain/kotlin/io/github/opletter/chesspg/MyApp.kt
@@ -0,0 +1,27 @@
+package io.github.opletter.chesspg
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.ui.modifiers.minHeight
+import com.varabyte.kobweb.core.App
+import com.varabyte.kobweb.silk.SilkApp
+import com.varabyte.kobweb.silk.components.layout.Surface
+import com.varabyte.kobweb.silk.components.style.common.SmoothColorStyle
+import com.varabyte.kobweb.silk.components.style.toModifier
+import com.varabyte.kobweb.silk.init.InitSilk
+import com.varabyte.kobweb.silk.init.InitSilkContext
+import org.jetbrains.compose.web.css.vh
+
+@InitSilk
+fun updateTheme(ctx: InitSilkContext) {
+    // Configure silk here
+}
+
+@App
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    SilkApp {
+        Surface(SmoothColorStyle.toModifier().minHeight(100.vh)) {
+            content()
+        }
+    }
+}
