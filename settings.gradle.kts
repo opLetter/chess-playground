a952324e0537736b6d167e5439183b203d067681
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-04-15 15:20:55 -0400

    Declare repositories in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,16 @@ pluginManagement {
     }
 }
 
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        mavenCentral()
+        google()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
+    }
+}
+
 rootProject.name = "chesspg"
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
