7346557c4f686d29969512f32247e821212d5c8e
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-08-09 12:16:29 -0400

    K2!

--- site/build.gradle.kts
+++ site/build.gradle.kts
@@ -1,9 +1,8 @@
-import com.varabyte.kobweb.gradle.application.extensions.AppBlock
 import com.varabyte.kobweb.gradle.application.util.configAsKobwebApplication
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.jetbrains.compose)
+    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.kobweb.application)
     alias(libs.plugins.kotlin.serialization)
 }
@@ -13,7 +12,6 @@ version = "1.0-SNAPSHOT"
 
 kobweb {
     app {
-        legacyRouteRedirectStrategy = AppBlock.LegacyRouteRedirectStrategy.DISALLOW
         index {
             description = "Powered by Kobweb"
         }
@@ -26,12 +24,12 @@ kotlin {
 
     sourceSets {
         commonMain.dependencies {
-            implementation(compose.runtime)
+            implementation(libs.compose.runtime)
             implementation(libs.kotlinx.serialization.json)
             implementation(projects.chess)
         }
         jsMain.dependencies {
-            implementation(compose.html.core)
+            implementation(libs.compose.html.core)
             implementation(libs.kobweb.core)
             implementation(libs.kobweb.silk)
             implementation(libs.silk.icons.fa)
